f74b87cff08c08b02307899b38919761ec17fa6b
M313K <99750900+M313K@users.noreply.github.com>
2023-03-26 12:07:34 +0100

    FirstCommitTest

--- backend/src/main/resources/DevelopmentTools/db.sql
+++ backend/src/main/resources/DevelopmentTools/db.sql
@@ -1,7 +1,15 @@
 DROP TABLE dbs;
 CREATE TABLE dbs(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    url VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255)
+                    id INT PRIMARY KEY AUTO_INCREMENT,
+                    url VARCHAR(255),
+                    username VARCHAR(255),
+                    password VARCHAR(255)
 );
+
+DROP TABLE pbs;
+CREATE TABLE pbs(
+                    id INT PRIMARY KEY AUTO_INCREMENT,
+                    Title VARCHAR(255),
+                    Description VARCHAR(255)
+);
+INSERT INTO pbs VALUES(1,'test','test');
