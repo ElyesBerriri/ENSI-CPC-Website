fe677b9d4674ac145c87ade72ccec8a4f5f0df97
ElyesBerriri <ilyesberriri@gmail.com>
2023-04-30 17:52:09 +0100

    Merge FRONTEND-FINAL into frontend
    +Change files' structure when upgrading FRONTEND-FINAL from angular 14 to angular 15

---
+++ frontend/src/app/contact/contact.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: 'contact.component.html',
+  styleUrls: ['contact.component.css'],
+})
+export class ContactComponent {
+  rawnd2h: string = ' '
+  raw72nb: string = ' '
+  rawnpps: string = ' '
+  rawqr77: string = ' '
+  rawi3bg: string = ' '
+  raw9bh4: string = ' '
+  raw9tlw: string = ' '
+  rawa2i9: string = ' '
+  raw14sw: string = ' '
+  rawb9e9: string = ' '
+  rawdvdw: string = ' '
+  rawubqu: string = ' '
+  rawa28h: string = ' '
+  raw6zzn: string = ' '
+}
---
+++ frontend/src/app/ressources/ressources.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-ressources',
+  templateUrl: 'ressources.component.html',
+  styleUrls: ['ressources.component.css'],
+})
+export class RessourcesComponent {
+  rawohdu: string = ' '
+  rawulxr: string = ' '
+  raw9u8r: string = ' '
+  rawt7ph: string = ' '
+  rawwgat: string = ' '
+  rawnssj: string = ' '
+  rawp0po: string = ' '
+  rawph9d: string = ' '
+  rawtobr: string = ' '
+  raw8rq7: string = ' '
+}
